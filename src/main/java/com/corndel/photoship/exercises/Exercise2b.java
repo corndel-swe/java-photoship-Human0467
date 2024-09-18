a87673ac16865e2005c90476d66832355cd52fb5
Human0467 <mustchjo@gmail.com>
2024-09-18 11:50:00 +0100

    refactored 2b to use stream/map/collect

--- src/main/java/com/corndel/photoship/exercises/Exercise2b.java
+++ src/main/java/com/corndel/photoship/exercises/Exercise2b.java
@@ -2,6 +2,7 @@ package com.corndel.photoship.exercises;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Exercise2b {
   // https://tech-docs.corndel.com/java/map-filter.html#map-and-filter-with-arrays
@@ -17,10 +18,15 @@ public class Exercise2b {
    * @returns {double[]} The list in MB
    */
   public static List<Double> convertFilesizes(List<Double> files) {
-    List<Double> filesMB = new ArrayList<Double>();
-      for (int i = 0; i < files.size(); i++) {
-        filesMB.add(i, files.get(i) / 1000);
-    }
-    return filesMB;
+//    List<Double> filesMB = new ArrayList<Double>();
+//      for (int i = 0; i < files.size(); i++) {
+//        filesMB.add(i, files.get(i) / 1000);
+//    }
+//    return filesMB;
+
+    //alternative!!
+    return files.stream().map(x -> x/1000).collect(Collectors.toList());
   }
+
+
 }
